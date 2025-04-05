64f8c215def45ff22ecae007a813896d8239d61d
@danilko.a.g <danilko.a.g@gmail.com>
2025-04-05 22:10:19 +0300

    WIP

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -56,9 +56,7 @@ func (handler *AuthHandler) Login() http.HandlerFunc {
 		// }
 
 		validation := validator.New()
-
 		err = validation.Struct(payload)
-
 		if err != nil {
 			response.Json(w, err.Error(), 402)
 			return
