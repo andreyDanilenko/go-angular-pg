b266dba9981d8977775ccdb7ef015800c3b36d60
@danilko.a.g <danilko.a.g@gmail.com>
2025-04-06 15:18:39 +0300

    add docker compose file

--- pkg/request/validate.go
+++ pkg/request/validate.go
@@ -6,5 +6,12 @@ import (
 
 func IsValid[T any](body T) error {
 	validation := validator.New()
+
+	// Для кастомных валидаций
+	// validate := validator.New()
+	// if err := validate.RegisterValidation("first_name", firstNameValidation); err != nil {
+	// 	log.Fatal("Failed to register validation:", err)
+	// }
+
 	return validation.Struct(body)
 }
