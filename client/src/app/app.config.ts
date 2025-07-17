d9b26cb8e499bf3332c3b8f0620eed84b0b313bb
@danilko.a.g <danilko.a.g@gmail.com>
2025-07-18 01:24:06 +0300

    add auth interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient() // <-- 💥 ЭТО ОБЯЗАТЕЛЬНО
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
