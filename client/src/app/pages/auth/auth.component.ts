011f4f255343b607e32720dc5f4b3081c76e3e76
@danilko.a.g <danilko.a.g@gmail.com>
2025-07-26 12:38:19 +0300

    update form for auth

--- client/src/app/pages/auth/auth.component.ts
+++ client/src/app/pages/auth/auth.component.ts
@@ -19,37 +19,65 @@ import { ButtonComponent } from '../../components/uikit/button/button.component'
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-
 export class AuthComponent {
   private fb = inject(FormBuilder);
   private api = inject(BaseApiService);
   private router = inject(Router);
   private auth = inject(AuthService);
 
+  isCodeVerification = false;
+  apiError: string | null = null;
+  userEmail: string = '';
+
   form: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]],
+    code: ['', [Validators.required, Validators.pattern(/^\d{6}$/)]]
   });
 
-  apiError: string | null = null;
+  ngOnInit() {
+    // Показываем только поля email и password при загрузке
+    this.form.get('code')?.disable();
+  }
 
   onSubmit() {
     this.apiError = null;
 
     if (this.form.invalid) return;
 
-    this.api.post('signin', this.form.value).subscribe({
-      next: (response: any) => {
-        if (response.token) {
-          this.auth.setToken(response.token);
-          this.router.navigate(['/']);
-        } else {
-          this.apiError = 'Некорректный ответ сервера';
+    if (!this.isCodeVerification) {
+      this.api.post('auth', {
+        email: this.form.value.email,
+        password: this.form.value.password
+      }).subscribe({
+        next: (response: any) => {
+            this.userEmail = this.form.value.email;
+            this.isCodeVerification = true;
+            this.form.get('email')?.disable();
+            this.form.get('password')?.disable();
+            this.form.get('code')?.enable();
+        },
+        error: (err) => {
+          this.apiError = err?.error?.message || 'Ошибка при входе';
         }
-      },
-      error: (err) => {
-        this.apiError = err?.error?.message || 'Ошибка при входе';
-      }
-    });
+      });
+    } else {
+      this.api.post('confirm', {
+        email: this.userEmail,
+        code: this.form.value.code
+      }).subscribe({
+        next: (response: any) => {
+          if (response.token) {
+            this.auth.setToken(response.token);
+            this.router.navigate(['/']);
+          } else {
+            this.apiError = 'Некорректный ответ сервера';
+          }
+        },
+        error: (err) => {
+          this.apiError = err?.error?.message || 'Ошибка при проверке кода';
+        }
+      });
+    }
   }
 }
