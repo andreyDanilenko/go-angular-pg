24bdbd56e2b668eb99aab2768f06610271d48c35
@danilko.a.g <danilko.a.g@gmail.com>
2025-08-22 10:05:36 +0300

    update base url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
-// export const environment = {
-//     production: true,
-//     apiUrl: 'https://lifedream.tech/api',
-//     socketUrl: 'wss://lifedream.tech/api'
-// };
-
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8080/api',
-    socketUrl: 'ws://localhost:8080/api'
+    production: true,
+    apiUrl: 'https://lifedream.tech/api',
+    socketUrl: 'wss://lifedream.tech/api'
 };
+
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:8080/api',
+//     socketUrl: 'ws://localhost:8080/api'
+// };
