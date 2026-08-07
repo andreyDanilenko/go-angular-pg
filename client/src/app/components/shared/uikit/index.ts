7afef61c852ee3563bfcd03e8a3ed56e652b7e53
andreyDanilenko <danilenko.a.g@mail.ru>
2026-08-07 21:38:38 +0300

    refactor: remove unused components and styles from the chat module to streamline the application structure and improve maintainability

--- client/src/app/components/shared/uikit/index.ts
+++
@@ -1,4 +0,0 @@
-export { ButtonComponent } from './button/button.component';
-export { InputComponent } from './input/input.component';
-export { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
-export { TooltipComponent } from './tooltip/tooltip.component';
--- client/src/app/stores/user-store/user.store.type.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../../core/types/user.model";
-
-export interface UserState {
-  users: User[];
-  adminUsers: User[];
-  currentUser: User | null;
-  loading: boolean;
-  error: string | null;
-}
